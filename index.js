77ae71a463bf8c9cab4bc70e44d1904abd07ba8e
Rex <42127311+i6t6@users.noreply.github.com>
2021-09-02 23:42:04 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ function tologin()
     var zm2 = document.getElementById("i2");
     var zm3 = document.querySelector("#username");
     var zm4 = document.querySelector("#password");
-    // zm4.setAttribute("type","text");
-    // var a3 = a2.match(/\u53d1\u8d27/)
+
     var a3 = zm.value.split(/\u5bc6\u7801\uff1a/)
     var a4 = a3[0].split(/\u8d26\u53f7\uff1a/)
     var a5=a4[1].match(/\d+/)
@@ -14,7 +13,7 @@ function tologin()
     //账号
     zm.value=a5;
     zm3.value=a5;
-    // zm4.value=a3[1];
+    
     var zm5 = document.querySelector("#i2");
 
     zm5.select();
